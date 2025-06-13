f8a65d5454fbbc3914c95f919d6088bd77695ffc
dbistel <ddbistel@gmail.com>
2025-06-12 19:54:17 -0600

    split and datatype check. flavor count shell started

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-
-
-
-
-
-
-
-
-
-const userInputString = prompt(
+const userInput = prompt(
   "Please enter flavors separated by commas.",
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
+typeof userInput === "string" //check to make sure input is a string not an int
+const flavors = userInput.split(","); // recognizes comma as a delimiter to separate in array
+
+function flavorCount (arr){
+    const flavors = {};
+    for (const num of arr){
+        if (num in flavors){
+            return true;
+        }
+        flavors[num] = num;
+    }
+    return false;
+};
+
+
+
+
+
+
+
